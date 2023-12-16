e2b5eb9d0d28710fe0734b8c08e22f5fd78bf740
Breno Ocanha <breno@dmxdesign.com.br>
2023-12-16 16:32:23 -0300

    feat: activity page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -138,3 +138,35 @@ export async function fetchUsers({
   }
 
 }
+
+export async function getActivity(userId: string) {
+
+  try {
+
+    connectToDB();
+
+    // Find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies / comments) from the 'children' field
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children)
+    }, [])
+
+    // Find all the threads that the user has replied to
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id'
+    })
+
+    return replies;
+
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message}`)
+  }
+
+}
